a4a05f050ee68a69b0efea0dc97ca3406beb21a7
Subin Choi <subin_97@naver.com>
2017-08-15 07:09:30 +0000

    팔로우 기능 구현(댓글X)

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_ownership, only: [:edit, :update, :destroy]
+  
+  def index
+    @posts = Post.all.order('created_at desc')
+    @posts_count = current_user.posts.length
+  end
+  
+  def new 
+  end
+  
+  def create
+    new_post = Post.new(user_id: current_user.id, content: params[:content])
+    if new_post.save
+      redirect_to root_path
+    else
+      redirect_to new_post_path
+    end
+  end
+  
+  def edit 
+  end
+  
+  def update
+    @post.content = params[:content]
+    if @post.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+  
+  private
+  def check_ownership
+     @post = Post.find_by(id: params[:id])
+     redirect_to root_path if @post.user_id != current_user.id
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  has_many :likes
+  has_many :liked_posts, through: :likes, source: :post
+  has_many :follower_relations, foreign_key: "followed_id", class_name: "Follow"
+  has_many :followers, through: :follower_relations, source: :follower
+  has_many :following_relation, foreign_key: "follower_id", class_name: "Follow"
+  has_many :followings, through: :following_relation, source: :followed
+  
+  def is_like?(post)
+    Like.find_by(user_id: self.id, post_id: post.id).present?
+  end
+end
